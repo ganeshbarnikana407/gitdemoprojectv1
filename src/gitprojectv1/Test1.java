29713b090775c012fae120cb0d1a2e8a39069e7e
ganesh barnikana <ganeshbarnikana407@gmail.com>
2025-03-18 12:06:51 +0530

    my first commit...

---
+++ src/gitprojectv1/Test1.java
@@ -0,0 +1,20 @@
+package gitprojectv1;
+
+public class Test1 {
+	
+	
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("start eating....");
+		}
+		else
+		{
+			System.out.println("do work....");
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gitprojectv1 {
+}
